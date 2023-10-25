9fd8a4d0dc0150ad544e6dacb61063fc8e19c837
mhtajan <marktajan@icloud.com>
2023-10-25 08:18:43 +0800

    updated library changes

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
 const fs = require('fs-extra');
 const path = require('path');
-const XLSX = require('xlsx');
+const ExcelJS = require('exceljs');
 const middleware = require('./middleware');
 
 const unprocessedFolder = './files/unprocessed';
 const processedFolder = './files/processed';
 const failedFolder = './files/failed';
 
-// Ensure the "processed" and "failed" folders exist
 fs.ensureDirSync(processedFolder);
 fs.ensureDirSync(failedFolder);
-fs.ensureDirSync(unprocessedFolder);
 
 const processExcelFile = async (file) => {
   const filePath = path.join(unprocessedFolder, file);
 
-  if (path.extname(file) !== '.xlsx'|| path.extname(file) !== '.XLSX') {
-    return; // Skip files with extensions other than .xlsx
+  if (path.extname(file) !== '.xlsx') {
+    return;
   }
 
   try {
-  
-    const workbook = XLSX.readFile(filePath);
-    const sheetName = workbook.SheetNames[0];
-    const worksheet = workbook.Sheets[sheetName];
-    const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-
-    const headers = data.shift();
-
-    // Create an array of objects with headers as keys and rows as values
-    const result = data.map((row) => {
-      const obj = {};
-      headers.forEach((header, index) => {
-        obj[header] = row[index];
-      });
-      return obj;
-    });
-
-
     console.log(`Processing: ${file}`);
+
+    const workbook = new ExcelJS.Workbook();
+    await workbook.xlsx.readFile(filePath);
+
+    const worksheet = workbook.getWorksheet(1);
+
+    const result = [];
+
+    // Assuming the first row contains headers
+    const headers = worksheet.getRow(1).values;
+
+    worksheet.eachRow((row, rowNumber) => {
+      if (rowNumber === 1) {
+        return; // Skip the header row
+      }
+
+      const dataItem = {};
+
+      row.eachCell((cell, colNumber) => {
+        const header = headers[colNumber];
+        dataItem[header] = cell.value;
+      });
+
+      result.push(dataItem);
+    });
 
     for (const dataItem of result) {
       const payload = {
@@ -54,42 +58,34 @@ const processExcelFile = async (file) => {
         contactNumber: dataItem['Contact Number'],
         addressOfInvoice: dataItem['Address to send the invoice'],
         emailAddress: dataItem['E-Mail Address'],
-      };
-
+      }
       // Add a 3-second delay before processing the next data item
       await new Promise((resolve) => setTimeout(resolve, 3000));
       await middleware.createObject(payload);
     }
 
-    // If processing is successful, move the file to the processed folder
     const destinationPath = path.join(processedFolder, file);
     fs.moveSync(filePath, destinationPath);
     console.log(`Processed: ${file}`);
   } catch (error) {
-    // If an error occurs during processing, move the file to the failed folder
     const destinationPath = path.join(failedFolder, file);
     fs.moveSync(filePath, destinationPath);
     console.error(`Failed: ${file}`);
-    console.error(error); // Log the error for debugging purposes
+    console.error(error);
   }
 };
 
-// Function to process new files in the unprocessed folder
 const processNewFilesInUnprocessedFolder = () => {
   fs.watch(unprocessedFolder, { persistent: true }, async (eventType, filename) => {
     if (eventType === 'rename' && filename && path.extname(filename) === '.xlsx') {
-      // Check if the file actually exists before processing it
       const filePath = path.join(unprocessedFolder, filename);
       if (fs.existsSync(filePath)) {
-        // Add a 3-second delay before processing the new file
         await new Promise((resolve) => setTimeout(resolve, 3000));
         processExcelFile(filename);
       }
     }
   });
 };
-
-
 
 fs.readdirSync(unprocessedFolder).forEach(processExcelFile);
 processNewFilesInUnprocessedFolder();
